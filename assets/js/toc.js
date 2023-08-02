8ceef5083fe651fcfbb3b8e4dab08032816a5e3b
dustjq1004 <dustjq1005@naver.com>
2023-08-02 13:34:34 +0900

    toc 기능 버그 수정
    intersectionObserver 에서 스크롤 값과 오프셋 값 비교 하여 구현되도록 변경

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -6,7 +6,6 @@ let tags = document.querySelectorAll('.blog-post h2, .blog-post h3');
 tocItems.forEach((item, index) => item.dataset.index = index);
 tags.forEach((tag, index) => tag.dataset.index = index);
 
-
 toc?.addEventListener('click', e => {
     if (e.target.matches('.toc-title')) {
         tocItems.forEach(target => target.classList.remove('toc-active'));
@@ -14,6 +13,26 @@ toc?.addEventListener('click', e => {
         tags[e.target.dataset.index].scrollIntoView({ behavior: "smooth", block: "start"});
     }
 });
+
+$(window).scroll(function() {
+    let headers = document.querySelectorAll('.blog-post h2, .blog-post h3');
+    let scrollPosition = $(this).scrollTop();
+    let selectedIndex = 0;
+    headers.forEach((header, index) => {
+        let offsetTop = header.offsetTop + 300; // 60은 헤더 높이 등 추가적으로 고려해야 할 값
+        if (scrollPosition >= offsetTop) {
+            // 해당 섹션이 보이는 경우 목차에서 해당 링크 강조
+            tocItems[selectedIndex].classList.remove('toc-active');
+            selectedIndex = header.dataset.index;
+            tocItems[selectedIndex].classList.add('toc-active');
+            // $(`.toc-title[data-index=${index}]`).addClass('toc-active');
+        } else {
+            // 해당 섹션이 보이지 않는 경우 목차에서 해당 링크 강조 제거
+            $(`.toc-title[data-index=${index}]`).removeClass('toc-active'); 
+        }
+    });
+  });
+  
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -34,4 +53,4 @@ const observer = new IntersectionObserver(entries => {
     })
 }, options);
 
-tags.forEach(tag => observer.observe(tag));
+//tags.forEach(tag => observer.observe(tag));
